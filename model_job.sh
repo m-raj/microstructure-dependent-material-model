da14b65da733a938cb17a4a2e44167f6d8087303
Mayank Raj <mayank227raj@gmail.com>
2025-11-17 13:29:27 -0800

    job

--- model_job.sh
+++ model_job.sh
@@ -20,15 +20,24 @@
 
 # Run the mpi job
 python model_main.py --run_id $1  \
-                            	--data_path data/2024-10-13_PC1D_process10_data.pkl \
+                            	--data_path "data/2024-10-13_PC1D_process10_data.pkl,
+data/2024-10-13_PC1D_process11_data.pkl,
+data/2024-10-13_PC1D_process12_data.pkl,
+data/2024-10-13_PC1D_process13_data.pkl,
+data/2024-10-13_PC1D_process14_data.pkl,
+data/2024-10-13_PC1D_process15_data.pkl,
+data/2024-10-13_PC1D_process16_data.pkl,
+data/2024-10-13_PC1D_process17_data.pkl,
+data/2024-10-13_PC1D_process18_data.pkl,
+data/2024-10-13_PC1D_process19_data.pkl"\
 			   	--epochs 5000 \
 				--lr 1e-3 \
 				--hidden_dim 300 \
-				--encoder_hidden_dim 512 \
-				--encoder_latent_dim 15 \
+				--encoder_hidden_dim 200 \
+				--encoder_latent_dim 30 \
 				--step 50 \
 				--n_samples 1000 \
-				--encoder_path encoder_run_1 \
+				--encoder_path encoder_run_4d \
 				--material_model m_independent \
 				--device cuda \
 				--batch_size 200 \
