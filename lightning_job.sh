b1a4e8d8b4477cc5851a4148d34f77dce2034cfe
Mayank Raj <mraj@caltech.edu>
2025-11-30 19:11:24 -0800

    lightning job

---
+++ lightning_job.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#SBATCH --mem-per-cpu=128G # memory per CPU core
+#SBATCH --nodes=1
+
+# Number of GPUs 
+# SBATCH --constraint="cascadelake"
+#SBATCH --partition gpu
+#SBATCH --gres gpu:1
+
+# Wall time: maxctivate base
+# Maximum allowed run time
+#SBATCH --time=5:30:00
+
+# Send email to user
+#SBATCH --mail-user=mraj@caltech.edu
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+
+# Run the mpi job
+args = (
+	--run_id "model_experiment_01" 
+	--mode "disable" 
+	--device "gpu" 
+#	--pca 
+#	--encoder_path "encoder_path"
+	--data_path "hmc.txt" 
+	--encoder_latent_dim 15 
+	--encoder_hidden_dim 128 
+	--encoder_epochs 100 
+	--encoder_batch_size 1000 
+	--encoder_lr 0.001 
+	--material_model "m_dependent_b" 
+	--hidden_dim 128 
+	--epochs 1000 
+	--lr 0.001 
+	--batch_size 1000 
+	--niv 1
+)
+
+python lightning_main.py "${args[@]}"
+	
+
