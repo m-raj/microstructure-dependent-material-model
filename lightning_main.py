4491a1db1796c127ba0a1e5d6aab13dd40dfa10f
Mayank Raj <mraj@caltech.edu>
2025-12-18 20:30:54 -0800

    msg

--- lightning_main.py
+++ lightning_main.py
@@ -132,7 +132,8 @@ if __name__ == "__main__":
 
         if args.encoder_path:
             print("Loading encoder weights from:", args.encoder_path)
-            file = glob.glob(f"{args.encoder_path}/ae_E*.pth")[0]
+            print(f"{args.encoder_path}/ae_E*.pth")
+            file = glob.glob(f"{args.encoder_path}/ae_E*.pth")
             lit_ae_E.load_state_dict(torch.load(file, weights_only=True))
             trainer_ae_E.test(lit_ae_E, test_dataloader)
             file = glob.glob(f"{args.encoder_path}/ae_nu*.pth")[0]
