0ca1509a9307c1bf8e506f7a78cc2c628eb1d311
Mayank Raj <mraj@caltech.edu>
2026-01-18 23:32:28 -0800

    jon

--- lightning_job.sh
+++ lightning_job.sh
@@ -10,7 +10,7 @@
 
 # Wall time: maxctivate base
 # Maximum allowed run time
-#SBATCH --time=10:30:00
+#SBATCH --time=20:30:00
 
 # Send email to user
 #SBATCH --mail-user=mraj@caltech.edu
@@ -35,7 +35,7 @@ args=(
 	--hidden_dim 150
 	--epochs 2000
 	--lr 0.001 
-	--batch_size 200 
+	--batch_size 32 
 	--niv 1
 	--step 50
 #	--freeze_encoder
