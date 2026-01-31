c73f318165c57ec98cb19831e9b6ff3a97e28da5
Mayank Raj <mraj@caltech.edu>
2026-01-31 13:06:39 -0800

    EVP

--- m_evp.py
+++ m_evp.py
@@ -43,20 +43,23 @@ class EnergyFunction(nn.Module):
 class InverseDissipationPotential(nn.Module):
     def __init__(self):
         super(InverseDissipationPotential, self).__init__()
-        # self.picnn1 = PartiallyInputConvex(y_dim=1, x_dim=1, z_dim=50, u_dim=50)
+        self.picnn1 = PartiallyInputConvex(y_dim=1, x_dim=1, z_dim=50, u_dim=50)
         # self.picnn2 = PartiallyInputConvex(y_dim=1, x_dim=1, z_dim=50, u_dim=50)
 
-    def forward(self, q, Y, n, edot_0):
-        potential = torch.mean(
-            torch.pow(torch.abs(q), n + 1) / (n + 1) * edot_0 * torch.pow(Y, -n),
-            dim=1,
-            keepdim=True,
-        )
+    def forward(self, q, m_features):
+
+        potential = self.picnn1(q.unsqueeze(-1), m_features)
+
+        # potential = torch.mean(
+        #     torch.pow(torch.abs(q), n + 1) / (n + 1) * edot_0 * torch.pow(Y, -n),
+        #     dim=1,
+        #     keepdim=True,
+        # )
         return potential.squeeze(-1)
 
-    def compute_derivative(self, q, Y, n, edot_0):
+    def compute_derivative(self, q, m_features):
 
-        potential = self(q, Y, n, edot_0)
+        potential = self(q, m_features)
         grad_q = torch.autograd.grad(
             potential.sum(), q, create_graph=True, retain_graph=True
         )[0]
@@ -85,20 +88,20 @@ class ViscoplasticMaterialModel(nn.Module):
         #     modes1=4, width=32, width_final=64, d_in=2, d_out=1, n_layers=3
         # )
 
-        # self.fnm3 = FNF1d(
-        #     modes1=4, width=32, width_final=64, d_in=1, d_out=1, n_layers=3
-        # )
+        self.fnm3 = FNF1d(
+            modes1=4, width=32, width_final=64, d_in=3, d_out=1, n_layers=3
+        )
         # self.fnm4 = FNF1d(
         #     modes1=4, width=32, width_final=64, d_in=2, d_out=1, n_layers=3
         # )
 
-    # def microstructure_encoder(self, E, nu):
-    #     microstructure = torch.stack((E, nu), dim=1)
-    #     features1 = self.fnm1(microstructure)
-    #     features2 = self.fnm2(microstructure)
-    #     features3 = self.fnm3(nu)
-    #     features4 = self.fnm4(microstructure)
-    #     return (features1, features3), (features2, features4)
+    def microstructure_encoder(self, Y, n, edot_0):
+        microstructure = torch.stack((Y, n, edot_0), dim=1)
+        #     features1 = self.fnm1(microstructure)
+        #     features2 = self.fnm2(microstructure)
+        features3 = self.fnm3(microstructure)
+        #     features4 = self.fnm4(microstructure)
+        return features3
 
     def forward(self, e, E, Y, n, edot_0):
 
@@ -109,10 +112,11 @@ class ViscoplasticMaterialModel(nn.Module):
             )
         ]
         m_features1 = 1 / torch.mean(1 / E, axis=1, keepdim=True)
+        m_features2 = self.microstructure_encoder(Y, n, edot_0)
 
         for i in range(0, e.shape[1]):
             s_eq, d = self.compute_energy_derivative(e[:, i], xi[i], m_features1)
-            kinetics = self.compute_dissipation_derivative(-d, Y, n, edot_0)
+            kinetics = self.compute_dissipation_derivative(-d, m_features2)
             stress.append(s_eq)
             if i < e.shape[1] - 1:
                 xi.append(xi[-1] + self.dt * kinetics)
@@ -123,5 +127,5 @@ class ViscoplasticMaterialModel(nn.Module):
     def compute_energy_derivative(self, u, v, m_features):
         return self.energy_function.compute_derivative(u, v, m_features)
 
-    def compute_dissipation_derivative(self, d, Y, n, edot_0):
-        return self.dissipation_potential.compute_derivative(d, Y, n, edot_0)
+    def compute_dissipation_derivative(self, d, m_features):
+        return self.dissipation_potential.compute_derivative(d, m_features)
